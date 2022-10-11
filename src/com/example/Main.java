adc95cdc592162ebfa260c9079f6220839c6f0c0
Will B <tryarc@comcast.net>
2022-10-11 16:02:59 -0400

    complete

---
+++ src/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        double pounds = 50;
+        double kilograms = pounds * 0.45359237;
+
+        System.out.println("Result: " + kilograms);
+    }
+}
